5122386fee2d575aff3e81a17b052aa70c614751
nikolay_prasolov <docum.magic0@gmail.com>
2026-01-20 13:36:00 +0300

    init

---
+++ layouts/layoutRD.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="page-rd">
+      <TheHeaderRD />
+      <main class="content">
+        <slot />
+      </main>
+      <TheFooterRD />
+    </div>
+    <ModalsContainer />
+  </div>
+</template>
+
+<script setup>
+import { ModalsContainer } from "vue-final-modal";
+import TheFooterRD from "~/components/global/footer/TheFooterRD.vue";
+import TheHeaderRD from "~/components/global/header/TheHeaderRD.vue";
+</script>
+<style lang="scss" scoped>
+@use "./../assets/css/vars" as v;
+@use "./../assets/css/mixins/text" as t;
+@use "sass-mq" as mq with (
+  $breakpoints: v.$breakpoints
+);
+
+.page-rd {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+  margin-top: 7rem; // отступ для fixed header
+
+  @include mq.mq($until: 640px) {
+    margin-top: 64px;
+  }
+}
+
+:deep(.footer-rd) {
+  margin-top: 56px;
+
+  @include mq.mq($until: 640px) {
+    margin-top: 96px;
+  }
+}
+</style>
